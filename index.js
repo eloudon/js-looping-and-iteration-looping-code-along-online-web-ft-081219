719ea926039f6ae255b15e578bce7c5922ffce55
Eric Loudon <loudon.eric@gmail.com>
2019-11-28 18:31:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards( arrayNames, birthday ) {
+  let thankYou = []
+  for ( let i = 0; i < arrayNames.length; i++ ) {
+      thankYou.push( `Thank you, ${arrayNames[i]}, for the wonderful ${birthday} gift!`)
+  }
+  return thankYou
+}
+
+
+
+  function countDown (i) {
+      while (i>-1) {
+          console.log(i--);
+      }
+  }
